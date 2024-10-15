4749fe202817a1bce220c778ddbf4f6d01fcc4e1
Oskar <open7software@gmail.com>
2024-10-15 18:58:01 +0200

    tried to improve weird tab/spacing

--- trashsystem2.cpp
+++ trashsystem2.cpp
@@ -37,100 +37,67 @@ int main (int argc, char **argv) {
 		usage_out(std::cerr);
         return EXIT_FAILURE;
     }
-	
-	int R_mut = 0;
-	int C_mut = 0;
-	int c_mut = 0;
-	int L_mut = 0;
-	int l_mut = 0;
-	int y_mut = 0;
-	int n_mut = 0;
-	int f_mut = 0;
-	int h_mut = 0;
-	bool y_used = false;
-	bool n_used = false;
-	bool v_used = false;
-	bool f_used = false;
-	bool t_used = false;
-	bool l_used = false;
-	bool L_used = false;
-	bool c_used = false;
-	bool C_used = false;
-	bool R_used = false;	
-	bool h_used = false;
-    int opt;
-	unsigned long long optarg_converted;
-	bool R_failed = false;
-	while ((opt = getopt(argc, argv, "ynvfatlLcCR:h")) != -1) {
-        switch (opt) {
-        case 'y':
 
-			y_mut = 1;
-			y_used = true; // YES on enter
+	int r_mtx = 0;
+	int C_mtx = 0;
+	int c_mtx = 0;
+	int L_mtx = 0;
+	int l_mtx = 0;
+	int y_mtx = 0;
+	int n_mtx = 0;
+	int f_mtx = 0;
+	int h_mtx = 0;
+	bool y_used;
+	bool n_used;
+	bool v_used;
+	bool f_used;
+	bool t_used;
+	bool l_used;
+	bool L_used;
+	bool c_used;
+	bool C_used;
+	bool R_used;
+	bool h_used;
+	int opt = 0;
+	while((opt = getopt(argc, argv, "ynvfatlLcCR:h")) != -1) {
 
-        break;
-        case 'n':
-
-			n_mut = 1;
-            n_used = true; // NO on enter
-
-        break;
-        case 'v':
+		switch(opt) {
+		case 'y':
 			
-            v_used = true; // Verbose debug mode
+			break;
+		case 'n':
 
-        break;
-        case 'f':
+			break;
+		case 'v':
 
-			f_mut = 1;
-            f_used = true; // choice will not ask, it will just say yes by default thus basically "forcing" it
-
-        break;
-	    case 't':
-
-			t_used = true;
+			break;
+		case 'f':
 			
-		break;
+			break;
+		case 't':
+
+			break;
 		case 'l':
 
-			l_mut = 1;
-			l_used = true;
-			
-		break;
+			break;
 		case 'L':
 
-			L_mut = 1;
-			L_used = true;
-			
-		break;
+			break;
 		case 'c':
-
-			c_mut = 1;
-			c_used = true;
 			
-		break;
+			break;
 		case 'C':
 
-			C_mut = 1;
-			C_used = true;
-			
-		break;
-		case 'R': {
+			break;
+		case 'R':
 
-			R_mut = 1;
-			R_used = true;
-			
-		}
-		break;
+			break;
 		case 'h':
 
-			h_mut = 1;
-			h_used = true;
-
-		break;
-        }
-    }
-
+			break;
+		}
+	}
+	
 	std::cout << "Smoketest" << std::endl;
 	std::cout << argc << std::endl;
 	std::cout << argv[0] << std::endl;
